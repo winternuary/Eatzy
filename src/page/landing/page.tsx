b0148de138c07fe89b3df94ac678fc67a4cf3ac4
zeroeuni <pkhj286@gmail.com>
2024-10-14 15:36:47 +0900

    Add: 메인 페이지 연결

--- src/page/landing/page.tsx
+++ src/page/landing/page.tsx
@@ -1,7 +1,22 @@
-import Image from "next/image";
+"use client";
+
 import * as styles from "./styles.css";
+import { useEffect, useState } from "react";
+import { Main } from "@/page/main/page";
 
 export const Landing = () => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsVisible(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (!isVisible) return <Main />;
+
   return (
     <div className={styles.container}>
       <p className={styles.Logo}>Eatzy</p>
