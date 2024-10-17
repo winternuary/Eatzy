6fdb94929e67019210b4336471bf525dcaae2593
zeroeuni <pkhj286@gmail.com>
2024-10-17 11:38:43 +0900

    chore : 컴포넌트 분리

---
+++ src/components/landing/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import * as styles from "./styles.css";
+
+export const Landing = () => {
+  return (
+    <div className={styles.container}>
+      <p className={styles.Logo}>Eatzy</p>
+    </div>
+  );
+};
